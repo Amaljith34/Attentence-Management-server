54e7d6098b54df5caaea4af2cb34131d40de4f84
Amaljith-aj <jith51541@gmail.com>
2024-12-25 15:49:28 +0530

    correction

--- controller/adminController/Leave/leave.js
+++ controller/adminController/Leave/leave.js
@@ -1,22 +1,45 @@
 import Leave from "../../../model/LeavesSchema/leavesSchema.js"
 
-export const handleRequest=async(req,res)=>{
-    const id=req.params.id
-    const {status}=req.body
-    const requestData=await Leave.findById(id)
-    if(!requestData){
-        return res.status(404).json({success:false,message:"request does not exist"})
-    }
-    if(status){
-        const ApproveRequest=await Leave.findByIdAndUpdate(id,{$set:{status:"approved"}},{new :true})
-        return res.status(200).json({success:true,message:"Leave request is Approved",data:ApproveRequest})
-    }
-    else{
-        const Rejectrequest=await Leave.findByIdAndUpdate(id,{$set:{status:"rejected"}},{new :true})
-        return res.status(400).json({success:true,message:"Leave request is Rejected",data:Rejectrequest})
+// export const handleRequest=async(req,res)=>{
+//     const id=req.params.id
+//     const {status}=req.body
+//     const requestData=await Leave.findById(id)
+//     if(!requestData){
+//         return res.status(404).json({success:false,message:"request does not exist"})
+//     }
+//     if(status){
+//         const ApproveRequest=await Leave.findByIdAndUpdate(id,{$set:{status:"approved"}},{new :true})
+//         return res.status(200).json({success:true,message:"Leave request is Approved",data:ApproveRequest})
+//     }
+//     else{
+//         const Rejectrequest=await Leave.findByIdAndUpdate(id,{$set:{status:"rejected"}},{new :true})
+//         return res.status(400).json({success:true,message:"Leave request is Rejected",data:Rejectrequest})
 
-    }
-}
+//     }
+// }
+export const handleRequest = async (req, res) => {
+    const id = req.params.id;
+    const { status } = req.body;
+  
+ 
+      const requestData = await Leave.findById(id);
+      if (!requestData) {
+        return res.status(404).json({ success: false, message: "Request does not exist" });
+      }
+  
+      const updatedRequest = await Leave.findByIdAndUpdate(
+        id,
+        { $set: { status: status ? "approved" : "rejected" } },
+        { new: true }
+      );
+  
+      const message = status
+        ? "Leave request has been approved"
+        : "Leave request has been rejected";
+  
+      return res.status(200).json({ success: true, message, data: updatedRequest });
+  };
+  
 
 export const getLeaves=async(req,res)=>{
     const { page = 1, limit = 10 } = req.query;
